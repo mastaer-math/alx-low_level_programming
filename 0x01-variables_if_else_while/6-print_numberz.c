fcbedd41fcc699972dd1576c14df5423d2073b0a
Dhikrullah <mustapha_dhikrullah@hotmail.com>
2022-04-09 03:28:01 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers single in base ten
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <='9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
