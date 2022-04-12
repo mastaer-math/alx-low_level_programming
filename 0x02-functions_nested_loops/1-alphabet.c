3a9f346027b6c4b73110ef9ccac426d5ba35d73f
Dhikrullah <mustapha_dhikrullah@hotmail.com>
2022-04-12 06:04:21 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+include "main.h"
+
+/**
+ * main - a function that prints alphabet, in lowercase 
+ * 
+ * Return: Always 0
+ */
+void print_alphabet(void)
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
+	
+
