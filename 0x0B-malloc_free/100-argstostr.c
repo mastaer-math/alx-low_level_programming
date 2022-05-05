5917301344f6cf76cfdca5e2f6a1e791da36a2f7
Mustapha <mustapha_dhikrullah@hotmail.com>
2022-05-05 03:33:47 -0700

    Write a function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
+**
  * argstostr - function that concatenates all the arguments of your program.
  *
  *
@@ -13,38 +13,37 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i = 0, j, longitude = 0, l, k;
-	char *string;
+        int i = 0, j, longitude = 0, l, k;
+        char *string;
 
-	if (ac == 0 || av == '\0')
-	return (NULL);
+        if (ac == 0 || av == '\0')
+        return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		j = 0;
-		while (av[i][j] != '\0')
-		{
-			j++;
-		}
-		longitude  +=  j + 1;
-	}
-	string = malloc(sizeof(char) * (longitude + 1));
+        for (i = 0; i < ac; i++)
+        {
+                j = 0;
+                while (av[i][j] != '\0')
+                {
+                        j++;
+                }
+                longitude  +=  j + 1;
+        }
+        string = malloc(sizeof(char) * (longitude + 1));
 
-	if (string == NULL)
-	{
-		return (NULL);
-	}
-	longitude = 0;
-	for (k = 0; k < ac; k++)
-	{
-		for (l = 0; av[k][l] != '\0'; l++)
-		{
-			*(string + longitude) = av[k][l];
-			longitude++;
-		}
-		*(string + longitude) = '\n';
-		longitude++;
-	}
+        if (string == NULL)
+        {
+                return (NULL);
+        }
+        longitude = 0;
+        for (k = 0; k < ac; k++)
+        {
+                for (l = 0; av[k][l] != '\0'; l++)
+                {
+                        *(string + longitude) = av[k][l];
+                        longitude++;
+                }
+                *(string + longitude) = '\n';
+                longitude++;
+        }
 
-	return (string);
-}
+"100-argstostr.c" 50L, 748C    
